0b63a48a3ba8e640b7fdd4036dad3cc47f49e454
Anna Szymula <ania.szymula505@gmail.com>
2020-10-28 16:38:19 -0500

    looper

--- looper/isloop.js
+++ looper/isloop.js
@@ -2,7 +2,31 @@
 
 //Complete this algo
 const isLoop = (linkedlist) => {
+    // console.log(linkedlist)
+    // need loop
+    //if (elements.next === val of any of nodes) {return false}
 
+    //if 2 different node have the same next, then it's going to have a loop
+    // check if the next's are equal
+
+    let looper = linkedlist.head
+    console.log(looper.next)
+    // let memo = {}
+    let memo = new Map()
+    while(looper.next) {
+        console.log("in while loop", looper.next)
+        // if (memo[looper]) {
+        if (memo.get(looper)) {
+            return true
+        } else {
+            // memo[looper] = true
+            memo.set(looper, true)
+            looper = looper.next
+        }
+    }
+    return false
+
+    
 };
 
 
