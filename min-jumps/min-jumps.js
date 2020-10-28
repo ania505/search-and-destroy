ab9a66f7d291c5b5cbabf23c70ef9511a7cf0332
Anna Szymula <ania.szymula505@gmail.com>
2020-10-28 17:14:16 -0500

    attempting minJumps

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -2,6 +2,21 @@
 
 // Complete this algo
 const minJumps = arr => {
+    let length = arr.length;
+    let jumpsCount = 0;
+    if (arr[0] === 0) {
+        return -1
+    }
+    if (length === 0 || length === 1) {
+        return 0
+    } else if (length === 2) {
+        return 1
+    }
+    let curr = arr[0]
+    let biggest = 0
+    for (let i=1; i<; i++) {
+        arr[i]
+    }
 
 };
 
