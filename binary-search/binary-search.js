5181611966750812d4d09644dd8a2d19abe9d19b
Jin young Choi <jinyoung@Jins-MacBook-Pro.local>
2020-10-29 05:13:56 +0900

    binarysearch

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,7 +2,30 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+  //base case: if array is zero, return false
+  //if array length is one, see if the element is target, return true/fase
+  //if neither split the array, array.length/2
+  //split the array left if less than middle, right if greater than middle
+  //run binarySearch again on split array
+
+  if (array.length === 0) return false;
+  if (array.length === 1) {
+    if (array[0] === target) {
+      return true;
+    } else {
+      return false;
+    }
+    // console.log(result);
+  } else {
+    const middle = Math.floor(array.length / 2);
+    const left = array.slice(0, middle);
+    const right = array.slice(middle);
+    if (target <= left[left.length - 1]) {
+      return binarySearch(left, target);
+    } else if (target >= right[0]) {
+      return binarySearch(right, target);
+    } else return false;
+  }
 };
 
 /*
@@ -14,4 +37,4 @@ const binarySearch = (array, target) => {
 
 */
 
-module.exports = binarySearch
+module.exports = binarySearch;
